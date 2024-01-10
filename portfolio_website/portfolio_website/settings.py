f2f9be07c08f809c900b3ea334994df0560eedef
sarcastic-pig <1005145q@gmail.com>
2024-01-09 19:14:05 -0600

    started homepage layout and styling

--- portfolio_website/portfolio_website/settings.py
+++ portfolio_website/portfolio_website/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'homepage',
+    'user_auth',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
