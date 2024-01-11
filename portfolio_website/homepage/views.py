1e33553e9277e712f2987c7cccf99e4299e013ec
sarcastic-pig <1005145q@gmail.com>
2024-01-11 15:33:26 -0600

    updated contact form

--- portfolio_website/homepage/views.py
+++ portfolio_website/homepage/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import ContactForm
+from django.http import HttpResponse
+from django.core.mail import send_mail
 # Create your views here.
 def home(request):
-    return render(request, "homepage/index.html" )
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['name']
+            from_email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+
+            send_mail(subject, message, from_email, ["hablequentin@gmail.com"])
+            return redirect('thanks/')
+    else:
+        form = ContactForm()
+    
+    return render(request, "homepage/index.html",  {"form":form} )
+
+
+def thanks(request):
+    return HttpResponse("Success!")
+
